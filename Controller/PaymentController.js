6653912e19d24741c4db7d9c0a1d842c88477499
AkashLokhande <akashlok97@gmail.com>
2025-10-12 20:12:36 +0530

    payment_getway_change

--- Controller/PaymentController.js
+++ Controller/PaymentController.js
@@ -1,16 +1,14 @@
-const Razorpay = require("razorpay");
-var crypto = require("crypto");
+const axios = require("axios");
 const OrdersModel = require("../Model/OrdersModel");
 
-const KEY_ID = "rzp_test_RB0WElnRLezVJ5";
-const SECRET_ID = "VLMCIrqKxRMNR9EcRcbL2UG8";
+const APP_ID = "TEST1036295297e9f86ce9c693eaff9525926301";
+const SECRET_KEY = "cfsk_ma_test_08bef886a0061e56bdd106d3cca35b73_eab9c7cf";
+const CASHFREE_SANDBOX_URL = "https://sandbox.cashfree.com/pg/orders";
 
-var instance = new Razorpay({ key_id: KEY_ID, key_secret: SECRET_ID });
-
+// Save new order to DB
 let _saveNewOrder = async (data) => {
- 
   try {
-    var newOrder = new OrdersModel({
+    const newOrder = new OrdersModel({
       order_id: data.order_id,
       name: data.name,
       mobile: data.mobile,
@@ -23,60 +21,76 @@ let _saveNewOrder = async (data) => {
     await newOrder.save();
     return true;
   } catch (error) {
+    console.error(error);
     return false;
   }
 };
 
-module.exports.genOrderId = (request, response) => {
-  let { amount } = request.body;
-  var options = {
-    amount: amount * 100, 
-    currency: "INR",
-    receipt: "order_rcptid_11",
-  };
-  instance.orders.create(options, function (err, order) {
-    if (err) {
-      response.status(500).send({ status: false });
+// Generate Cashfree order
+module.exports.genOrderId = async (req, res) => {
+  try {
+    const { amount, customer_name, customer_email, customer_phone } = req.body;
+
+    const orderPayload = {
+      order_id: `ORDER_${Date.now()}`, // unique order id
+      order_amount: amount,
+      order_currency: "INR",
+      customer_details: {
+        customer_name,
+        customer_email,
+        customer_phone,
+      },
+      order_meta: {
+        return_url: "https://yourfrontend.com/payment-success", // change to your frontend route
+        notify_url: "https://yourbackend.com/payment/callback", // optional
+      },
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+      "x-client-id": APP_ID,
+      "x-client-secret": SECRET_KEY,
+    };
+
+    const response = await axios.post(CASHFREE_SANDBOX_URL, orderPayload, { headers });
+
+    if (response.data.status === "OK") {
+      res.status(200).send({ status: true, order: response.data });
     } else {
-      response.status(200).send({ status: true, order });
+      res.status(400).send({ status: false, message: response.data.message });
     }
-  });
-};
-
-module.exports.verifyPayment = async (request, response) => {
-  let data = request.body;
-  let { payment_id, order_id, signature } = data;
-  let body = order_id + "|" + payment_id;
-
-  var expectedSignature = crypto
-    .createHmac("sha256", SECRET_ID)
-    .update(body.toString())
-    .digest("hex");
-
-  console.log(signature, expectedSignature);
-  if (expectedSignature === signature) {
-    data["payment_status"] = true;
-    await _saveNewOrder(data);
-    response.status(200).send({
-      status: true,
-    });
-  } else {
-    response.status(200).send({
-      status: false,
-    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false, error });
   }
 };
 
-module.exports.OrderList = async (request, response) => {
-
-   let {email} = request.params
-
-  let result = await OrdersModel.find({email});
-  response.send({
-    status: true,
-    my_order: result,
-  });
+// Verify payment (Cashfree sends a callback)
+module.exports.verifyPayment = async (req, res) => {
+  try {
+    const data = req.body;
+    // Cashfree webhook will send payment status. For testing, you can assume success.
+    if (data.order_status === "PAID") {
+      data.payment_status = true;
+      await _saveNewOrder(data);
+      res.status(200).send({ status: true });
+    } else {
+      res.status(200).send({ status: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false });
+  }
 };
 
-//63899916ec6ac0a4b2cbe197620eef1c515df86dc1a2425080048ae35d80bfc1
-//63899916ec6ac0a4b2cbe197620eef1c515df86dc1a2425080048ae35d80bfc1
+// List orders by email
+module.exports.OrderList = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const result = await OrdersModel.find({ email });
+    res.send({ status: true, my_order: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false });
+  }
+};
