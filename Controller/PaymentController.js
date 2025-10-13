818c5346588fbf5bb66c65032fb24fbac97d19ba
AkashLokhande <akashlok97@gmail.com>
2025-10-13 18:10:49 +0530

    ordrer_changes

--- Controller/PaymentController.js
+++ Controller/PaymentController.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const Razorpay = require("razorpay");
-var crypto = require("crypto");
+const crypto = require("crypto");
 const OrdersModel = require("../Model/OrdersModel");
 
 const KEY_ID = process.env.RAZORPAY_KEY_ID;
 const SECRET_ID = process.env.RAZORPAY_SECRET_ID;
 
-
 var instance = new Razorpay({ key_id: KEY_ID, key_secret: SECRET_ID });
 
 let _saveNewOrder = async (data) => {
- 
   try {
     var newOrder = new OrdersModel({
       order_id: data.order_id,
@@ -29,24 +27,26 @@ let _saveNewOrder = async (data) => {
   }
 };
 
-module.exports.genOrderId = (request, response) => {
-  let { amount } = request.body;
+// Generate Order ID
+module.exports.genOrderId = (req, res) => {
+  let { amount } = req.body;
   var options = {
-    amount: amount * 100, 
+    amount: amount * 100,
     currency: "INR",
     receipt: "order_rcptid_11",
   };
   instance.orders.create(options, function (err, order) {
     if (err) {
-      response.status(500).send({ status: false });
+      res.status(500).send({ status: false });
     } else {
-      response.status(200).send({ status: true, order });
+      res.status(200).send({ status: true, order });
     }
   });
 };
 
-module.exports.verifyPayment = async (request, response) => {
-  let data = request.body;
+// Verify Payment
+module.exports.verifyPayment = async (req, res) => {
+  let data = req.body;
   let { payment_id, order_id, signature } = data;
   let body = order_id + "|" + payment_id;
 
@@ -55,19 +55,22 @@ module.exports.verifyPayment = async (request, response) => {
     .update(body.toString())
     .digest("hex");
 
-  console.log(signature, expectedSignature);
   if (expectedSignature === signature) {
     data["payment_status"] = true;
     await _saveNewOrder(data);
-    response.status(200).send({
-      status: true,
-    });
+    res.status(200).send({ status: true });
   } else {
-    response.status(200).send({
-      status: false,
-    });
+    res.status(200).send({ status: false });
   }
 };
 
-//63899916ec6ac0a4b2cbe197620eef1c515df86dc1a2425080048ae35d80bfc1
-//63899916ec6ac0a4b2cbe197620eef1c515df86dc1a2425080048ae35d80bfc1
+// Get Orders by Email
+module.exports.OrderList = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const orders = await OrdersModel.find({ email });
+    res.status(200).send({ status: true, orders });
+  } catch (err) {
+    res.status(500).send({ status: false, error: err.message });
+  }
+};
